d0613b0e92bc94339007c41f5b3b73857a4f31c9
Grzegorz Miąskiewicz <grzegorz@miaskiewicz.com>
2016-04-08 13:05:41 -0700

    mo checkinx

--- main.js
+++ main.js
@@ -17,6 +17,54 @@ $(document).on('ready', function() {
 	$("#menu").removeClass('hidden');
 });
 
+if ($(".combs-container .comb").length) {
+	$(".combs-container").on('click', '.comb', function() {
+		var el = $(this);
+		var bodyEl = $(el.find('.body')[0]);
+		var iconEl = $(el.find('.icon')[0]);
+		var titleEl = $(el.find('.title')[0]);
+
+		if (! el.hasClass('active')) {
+			iconEl.animate({
+				"top": "10px",
+	  			"width": "19px",
+	  			"height": "19px"
+			}, 200);
+
+			titleEl.animate({
+				"font-size": "14px",
+	  			"padding": "0 60px",
+	  			"top": "5px"
+			}, 200);
+
+			bodyEl.animate({
+				"opacity": 1
+			}, 200, function() {
+				el.addClass('active');
+			});
+		} else {
+			iconEl.animate({
+				"top": "65px",
+	  			"width": "38px",
+	  			"height": "38px"
+			}, 200);
+
+			titleEl.animate({
+				"font-size": "18px",
+	  			"padding": "0 0",
+	  			"top": "65px"
+			}, 200);
+
+			bodyEl.animate({
+				"opacity": 0
+			}, 200, function() { 
+				el.removeClass('active');
+			});
+		}
+	});
+}
+
+
 if ($(".contact-form").length) { 
 	$(".contact-form").on('click', "span.wpcf7-not-valid-tip", function() {
 		$($(this).parent().find('input, textarea')[0]).focus();

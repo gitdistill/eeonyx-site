e8f6d38b2097b5cde40036786ee69c2af779fbd4
Jasper Speicher <jasper.speicher@gmail.com>
2016-05-23 13:04:00 -0700

    Remove commented code

--- footer.php
+++ footer.php
@@ -1,39 +1,11 @@
-<!--
-<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php if ( has_nav_menu( 'primary' ) ) : ?>
-				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'primary',
-							'menu_class'     => 'primary-menu',
-						 ) );
-					?>
-				</nav>
-			<?php endif; ?>
-
-			<?php if ( has_nav_menu( 'social' ) ) : ?>
-				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'social',
-							'menu_class'     => 'social-links-menu',
-							'depth'          => 1,
-							'link_before'    => '<span class="screen-reader-text">',
-							'link_after'     => '</span>',
-						) );
-					?>
-				</nav>
-			<?php endif; ?>
--->
-			
-					
-				<?php if (!is_home() && !is_404()) { ?>
-				<div class="footer">
-					<div class="logo"></div>
-				</div>
-				<?php } ?>
-			</main>
+				</main>
+			</div>
 		</div>
+		<?php if (!is_home() && !is_404()) { ?>
+		<div class="footer">
+			<div class="logo"></div>
+		</div>
+		<?php } ?>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 		<script type="text/javascript" src="/wp-content/themes/eeonyx-wp-theme/js/autosize.min.js"></script>
 		<script type="text/javascript" src="/wp-content/themes/eeonyx-wp-theme/vendor/slideout.js"></script>

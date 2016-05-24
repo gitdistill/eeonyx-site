ace5210d3fd90eb8d85874ab602fee3c97eb7a04
Jasper Speicher <jasper.speicher@gmail.com>
2016-05-24 13:25:39 -0700

    Improve About styles

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -44,7 +44,7 @@
       $hide_image_on_mobile = $row['hide_image_on_mobile']
       ?>
 
-      <div class="paragraph-with-image row">
+      <div class="paragraph-with-image row row-<?=$key?>">
         <div class="col-sm-4 image <?=$swap_columns?'col-sm-push-8':'';?>">
           <?php if( $row['image'] ) { ?>
             <img src="<?=$row['image']['url']?>" class="<?=$hide_image_on_mobile?'hidden-xs':'';?>" alt="<?=$row['image']['alt']?>">

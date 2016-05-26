2580ccc70857c725434960a0cad7eb1c1a90a003
Jasper Speicher <jasper.speicher@gmail.com>
2016-05-25 18:09:34 -0700

    inclide all scripts needed in footer

--- footer.php
+++ footer.php
@@ -10,8 +10,23 @@
 			<div class="logo"></div>
 		</div>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script type="text/javascript" src="./vendor/jquery.slimscroll.min.js"></script>
+		<script type="text/javascript" src="./jquery.fullPage.min.js"></script>
 		<script type="text/javascript" src="/wp-content/themes/eeonyx-wp-theme/js/autosize.min.js"></script>
-		<script type="text/javascript" src="/wp-content/themes/eeonyx-wp-theme/vendor/slideout.js"></script>
 		<script type="text/javascript" src="/wp-content/themes/eeonyx-wp-theme/main.js"></script>
+		<script type='text/javascript'>
+		/* <![CDATA[ */
+		var _wpcf7 = {"loaderUrl":"http:\/\/eeonyx.com\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","recaptchaEmpty":"Please verify that you are not a robot.","sending":"Sending ..."};
+		/* ]]> */
+		</script>
+		<script type='text/javascript'>
+		/* <![CDATA[ */
+		var screenReaderText = {"expand":"expand child menu","collapse":"collapse child menu"};
+		/* ]]> */
+		</script>
+		
+		<!-- WP footer -->
+		<?php wp_footer(); ?>
+
 	</body>
 </html>

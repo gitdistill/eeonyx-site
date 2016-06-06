c836fef8b8ebc7c680ccf13abf3843a7b2623ba8
Jasper Speicher <jasper.speicher@gmail.com>
2016-06-06 12:43:21 -0700

    no message

--- main.js
+++ main.js
@@ -270,6 +270,9 @@ var initFullPage = function() {
       });
 
       //resize to the actual outer dims, minus the chrome, immediately
+      console.log( window.height );
+      console.log( window );
+
       resizeSections( window.width, window.height );
 
     }
